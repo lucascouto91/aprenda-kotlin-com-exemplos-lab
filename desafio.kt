3e8ca174ab9db5617841974d989bef610056751f
Lucas Couto <42366128+lucascouto91@users.noreply.github.com>
2023-10-30 18:26:08 -0300

    fix: fun matricular

--- desafio.kt
+++ desafio.kt
@@ -12,6 +12,7 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     
     fun matricular(usuario: Usuario) {
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
