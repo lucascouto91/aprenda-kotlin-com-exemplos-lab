65ac72ef12137e6751c849cce96b8a7ac7a780cc
Lucas Couto <42366128+lucascouto91@users.noreply.github.com>
2023-10-30 18:43:29 -0300

    test: add content to classes

--- desafio.kt
+++ desafio.kt
@@ -30,4 +30,18 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João","joao@kotlin.com", 38)
+    val usuario2 = Usuario("Maria","maria@kotlin.com",35)
+
+    val conteudo1 = ConteudoEducacional("Introducao ao Kotlin", 90, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Programacao Orientada a Objetos", 120, Nivel.DIFICIL)
+
+    val formacao1 = Formacao("Desenvolvimento Android", mutableListOf(conteudo1, conteudo2))
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+
+    formacao1.adicionarConteudo(ConteudoEducacional("Estruturas de Controle de Fluxo e Coleções em Kotlin", 110, Nivel.INTERMEDIARIO))
+    formacao1.adicionarConteudo(ConteudoEducacional("O Poder das Funções em Kotlin", 100, Nivel.DIFICIL))
+    
+    formacao1.exibirEstatisticas()
 }
