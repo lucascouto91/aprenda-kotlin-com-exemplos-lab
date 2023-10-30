c65a7f4149eee77d84f87d18e2ad38ab11cbab46
Lucas Couto <42366128+lucascouto91@users.noreply.github.com>
2023-10-30 18:24:09 -0300

    feat: add new properties to the ConteudoEducacional class

--- desafio.kt
+++ desafio.kt
@@ -4,7 +4,7 @@ enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario(val nome: String, val email: String, val idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
