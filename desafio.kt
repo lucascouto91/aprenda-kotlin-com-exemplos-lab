1e8eb7e802e1c92babedaa99e1a369079ecf3420
Lucas Couto <42366128+lucascouto91@users.noreply.github.com>
2023-10-30 18:28:21 -0300

    feat: add fun adiocionarConteudo

--- desafio.kt
+++ desafio.kt
@@ -14,6 +14,9 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
         inscritos.add(usuario)
     }
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
 }
 
 fun main() {
